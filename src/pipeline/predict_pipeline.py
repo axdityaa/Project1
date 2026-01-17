7200bdbb27b5ec2a24dec0ab83e9c8dce5642240
Aditya <adityadube9284@gmail.com>
2026-01-18 02:44:18 +0530

    Change

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from src.exception import CustomException
 from src.utils import load_object
+from src.components.model_trainer import ModelTrainer
 
 
 class PredictPipeline:
